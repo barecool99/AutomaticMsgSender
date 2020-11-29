f90ae4f63beb2abd6751aeca89f6cca0663c4786
barecool99 <35775587+barecool99@users.noreply.github.com>
2020-11-29 12:29:27 +0000

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-cellphone = "+447427609378"
-twilio_number = "+19384448287"
-twilio_account = "AC8f5da71e92902c506ce729bb0189e133"
-twilio_token = "247d3053a6bbbf02e7033a6a2e28552d"
+cellphone = "+put your cell phone no here"
+twilio_number = "+put your twillio number here"
+twilio_account = "put your twillio account no here"
+twilio_token = "put your twillio token here"
